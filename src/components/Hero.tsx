818e13d4b07a6158e116845b9002ebe8b5d40127
Adalberto Kutuxidis <adalberto.kutuxidis@gmail.com>
2026-03-17 16:32:28 +0100

    fix: make scroll hint arrow navigate to About section on click

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,9 +16,9 @@ export function Hero() {
       </div>
 
       {/* Scroll hint */}
-      <div className="absolute bottom-8 left-1/2 -translate-x-1/2 z-10">
+      <a href="#about" className="absolute bottom-8 left-1/2 -translate-x-1/2 z-10 cursor-pointer">
         <div className="w-4 h-4 border-b-2 border-r-2 border-soul-ember rotate-45 animate-bounce opacity-60" />
-      </div>
+      </a>
     </section>
   )
 }
